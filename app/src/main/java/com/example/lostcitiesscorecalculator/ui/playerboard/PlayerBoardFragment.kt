cf65404176b0648b6adbefb1cd18e0055e877dbf
askariya <arunaluk@yahoo.ca>
2024-07-06 11:59:38 -0600

    Add basic scoring without wagers and colours for total columns

--- app/src/main/java/com/example/lostcitiesscorecalculator/ui/playerboard/PlayerBoardFragment.kt
+++ app/src/main/java/com/example/lostcitiesscorecalculator/ui/playerboard/PlayerBoardFragment.kt
@@ -1,19 +1,16 @@
 package com.example.lostcitiesscorecalculator.ui.playerboard
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.GridLayout
-import android.widget.ToggleButton
-import androidx.appcompat.view.ContextThemeWrapper
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.lostcitiesscorecalculator.R
 import com.example.lostcitiesscorecalculator.databinding.FragmentPlayerboardBinding
+import com.google.android.material.card.MaterialCardView
 
 class PlayerBoardFragment : Fragment() {
 
@@ -34,7 +31,7 @@ class PlayerBoardFragment : Fragment() {
         _binding = FragmentPlayerboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        addButtonsToGridLayout()
+        setupGridLayout()
         return root
     }
 
@@ -43,7 +40,10 @@ class PlayerBoardFragment : Fragment() {
         _binding = null
     }
 
-    private val button_colours = mutableMapOf<Int, String>().apply {
+    private val totalButtonRows = 10;
+    private val totalButtonColumns = 6;
+
+    private val buttonColours = mutableMapOf<Int, String>().apply {
         this[0] = "yellow"
         this[1] = "white"
         this[2] = "blue"
@@ -52,7 +52,7 @@ class PlayerBoardFragment : Fragment() {
         this[5] = "purple"
     }
 
-    private val light_colours = mutableMapOf<Int, String>().apply {
+    private val lightColours = mutableMapOf<Int, String>().apply {
         this[0] = "light_yellow"
         this[1] = "light_white"
         this[2] = "light_blue"
@@ -75,6 +75,21 @@ class PlayerBoardFragment : Fragment() {
         return if (index != -1) gridLayout.getChildAt(index) as Button else null
     }
 
+    private fun findTextView(row: Int, column: Int): TextView? {
+        val gridLayout = binding.boardGrid
+        val rowCount = gridLayout.rowCount
+        val columnCount = gridLayout.columnCount
+
+        if (row < 0 || row >= rowCount || column < 0 || column >= columnCount) {
+            return null // Invalid row or column
+        }
+
+        val index = row * columnCount + column
+
+        val cardView = if (index != -1) gridLayout.getChildAt(index) as MaterialCardView else null
+        return if (index != -1) cardView?.getChildAt(0) as TextView else null;
+    }
+
     private fun getColorFromString(colorName : String?) : Int {
         var colour = "white"
         if (colorName != null)
@@ -84,7 +99,7 @@ class PlayerBoardFragment : Fragment() {
         return ContextCompat.getColor(requireContext(), colorResourceId)
     }
 
-    private fun toggleButtonState(button: Button, selectedColour: Int, unselectedColour: Int, selectedTextColour: Int) {
+    private fun toggleButtonState(column : Int, button: Button, selectedColour: Int, unselectedColour: Int, selectedTextColour: Int) {
         val isButtonOn = button.tag as? Boolean ?: false
 
         if (isButtonOn) {
@@ -96,26 +111,60 @@ class PlayerBoardFragment : Fragment() {
             button.setTextColor(selectedTextColour)
             button.tag = true
         }
+
+        checkAndToggleTotalTextViewState(column)
     }
 
-    private fun addButtonsToGridLayout() {
-        val totalRows = 10;
-        val totalColumns = 6;
+    private fun checkAndToggleTotalTextViewState(column : Int)
+    {
+        var columnScore = 0
+        var selectedButtonCount = 0
+        val totalTextView: TextView = findTextView(10, column)!!
 
-        for (col in 0 until totalColumns)
+        //TODO Calculate score for wager buttons
+
+        // Calculate Score for numbered buttons
+        for (row in 1 until totalButtonRows)
         {
-            val buttonColor = getColorFromString(button_colours[col])
+            val rowButton = findButton(row, column)
+            val buttonIsSelected = rowButton?.tag as? Boolean ?: false
+
+            if (buttonIsSelected)
+            {
+                columnScore += (row + 1)
+                selectedButtonCount++
+            }
+        }
+
+        if (selectedButtonCount > 0) {
+            columnScore -= 20
+            totalTextView.setBackgroundColor(getColorFromString(lightColours[column]))
+        } else {
+            totalTextView.setBackgroundColor(getColorFromString("light_grey"))
+        }
+
+        if (selectedButtonCount >= 8)
+            columnScore += 20
+
+        totalTextView.text = columnScore.toString()
+    }
+
+    private fun setupGridLayout() {
+        for (col in 0 until totalButtonColumns)
+        {
+            val buttonColor = getColorFromString(buttonColours[col])
             val unselectedBackgroundColor = getColorFromString("dark_grey")
             val selectedTextColor = getColorFromString("black")
 
+            // Set the OnClickListener and colours for each button
             // Skip the wager row
-            for (row in 1 until totalRows)
+            for (row in 1 until totalButtonRows)
             {
                 val button: Button? = findButton(row, col)
 
                 button?.setTextColor(buttonColor)
                 button?.setOnClickListener {
-                    toggleButtonState(button, buttonColor, unselectedBackgroundColor, selectedTextColor)
+                    toggleButtonState(col, button, buttonColor, unselectedBackgroundColor, selectedTextColor)
                 }
             }
         }
