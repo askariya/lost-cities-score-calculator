c1b28d59ffd126293d727dcdd3ac36dcef263217
askariya <arunaluk@yahoo.ca>
2024-07-06 16:42:46 -0600

    Beautify wager buttons

--- app/src/main/java/com/example/lostcitiesscorecalculator/ui/playerboard/PlayerBoardFragment.kt
+++ app/src/main/java/com/example/lostcitiesscorecalculator/ui/playerboard/PlayerBoardFragment.kt
@@ -1,14 +1,18 @@
 package com.example.lostcitiesscorecalculator.ui.playerboard
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.lostcitiesscorecalculator.R
 import com.example.lostcitiesscorecalculator.databinding.FragmentPlayerboardBinding
 import com.google.android.material.card.MaterialCardView
 
@@ -40,8 +44,13 @@ class PlayerBoardFragment : Fragment() {
         _binding = null
     }
 
-    private val totalButtonRows = 10;
-    private val totalButtonColumns = 6;
+    private val totalButtonRows = 10
+    private val totalButtonColumns = 6
+
+    private var wButtonTopPadding = 0
+    private var wButtonBottomPadding = 0
+    private var wButtonLeftPadding = 0
+    private var wButtonRightPadding = 0
 
     private val buttonColours = mutableMapOf<Int, String>().apply {
         this[0] = "yellow"
@@ -73,6 +82,17 @@ class PlayerBoardFragment : Fragment() {
         val index = row * columnCount + column
 
         return if (index != -1) gridLayout.getChildAt(index) as Button else null
+    }
+
+    private fun findImageButton(column: Int): ImageButton? {
+        val gridLayout = binding.boardGrid
+        val columnCount = gridLayout.columnCount
+
+        if (column < 0 || column >= columnCount) {
+            return null // Invalid row or column
+        }
+
+        return gridLayout.getChildAt(column) as ImageButton
     }
 
     private fun findTextView(row: Int, column: Int): TextView? {
@@ -115,7 +135,7 @@ class PlayerBoardFragment : Fragment() {
         checkAndToggleTotalTextViewState(column)
     }
 
-    private fun toggleWagerButtonState(column: Int, wagerButton: Button, selectedColour: Int, unselectedColour: Int, selectedIconColor : Int)
+    private fun toggleWagerButtonState(column: Int, wagerButton: ImageButton, selectedColour: Int, unselectedColour: Int, selectedIconColor : Int)
     {
         val wagerCount = wagerButton.tag as? Int ?: 0
 
@@ -123,17 +143,25 @@ class PlayerBoardFragment : Fragment() {
         {
             0 -> {
                 wagerButton.tag = 1
-                wagerButton.setBackgroundColor(selectedColour)
+                wagerButton.backgroundTintList = ColorStateList.valueOf(selectedColour)
+                wagerButton.setColorFilter(selectedIconColor)
             }
             1 -> {
                 wagerButton.tag = 2
+                wagerButton.setImageResource(R.drawable.ic_wager2)
+                wagerButton.scaleType = ImageView.ScaleType.CENTER_CROP
             }
             2 -> {
                 wagerButton.tag = 3
+                wagerButton.setImageResource(R.drawable.ic_wager3_alternate)
+                wagerButton.setPadding(15,15,15,15)
             }
             3 -> {
                 wagerButton.tag = 0
-                wagerButton.setBackgroundColor(unselectedColour)
+                wagerButton.backgroundTintList = ColorStateList.valueOf(unselectedColour)
+                wagerButton.setImageResource(R.drawable.ic_wager)
+                wagerButton.setColorFilter(selectedColour)
+                wagerButton.scaleType = ImageView.ScaleType.CENTER_INSIDE
             }
         }
 
@@ -147,7 +175,7 @@ class PlayerBoardFragment : Fragment() {
         val totalTextView: TextView = findTextView(10, column)!!
 
         // Calculate the corresponding multiple based on the number of wager cards
-        val wagerButton = findButton(0, column)
+        val wagerButton = findImageButton(column)
         val wagerButtonMultiple = (wagerButton?.tag as? Int ?: 0) + 1
 
         if (wagerButtonMultiple > 1)
@@ -182,15 +210,21 @@ class PlayerBoardFragment : Fragment() {
     }
 
     private fun setupGridLayout() {
+        val unselectedBackgroundColor = getColorFromString("dark_grey")
+        val selectedTextColor = getColorFromString("black")
+
         for (col in 0 until totalButtonColumns)
         {
             val buttonColor = getColorFromString(buttonColours[col])
-            val unselectedBackgroundColor = getColorFromString("dark_grey")
-            val selectedTextColor = getColorFromString("black")
 
             // Set the OnClickListener and icons for the Wager button
-            val wagerButton : Button? = findButton(0, col)
+            val wagerButton : ImageButton? = findImageButton(col)
+            wButtonTopPadding = wagerButton?.paddingTop!!
+            wButtonBottomPadding = wagerButton?.paddingBottom!!
+            wButtonLeftPadding = wagerButton?.paddingLeft!!
+            wButtonRightPadding = wagerButton?.paddingRight!!
             wagerButton?.tag = 0
+            wagerButton?.setColorFilter(buttonColor)
             wagerButton?.setOnClickListener {
                 toggleWagerButtonState(col, wagerButton, buttonColor, unselectedBackgroundColor, selectedTextColor)
             }
