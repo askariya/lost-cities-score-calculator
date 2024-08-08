23bf60b2c752c8477cd9ab3616b1fe33b25012ec
askariya <arunaluk@yahoo.ca>
2024-08-08 16:42:08 -0600

    Fix settings theme colour issues yet again

--- app/src/main/java/com/example/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
@@ -5,13 +5,6 @@ import androidx.preference.PreferenceFragmentCompat
 import com.example.lostcitiesscorecalculator.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        // Apply the custom preference theme before the super call
-        context?.theme?.applyStyle(R.style.CustomPreferenceTheme, true)
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
