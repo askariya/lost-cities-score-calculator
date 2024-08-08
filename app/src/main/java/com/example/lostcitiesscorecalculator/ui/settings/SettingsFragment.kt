da82463b55c1cb6141bf16efdbfc14fda1d30a9d
askariya <arunaluk@yahoo.ca>
2024-08-08 15:50:12 -0600

    Fix settings page UI issues

--- app/src/main/java/com/example/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
@@ -6,6 +6,12 @@ import com.example.lostcitiesscorecalculator.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Apply the custom preference theme before the super call
+        context?.theme?.applyStyle(R.style.CustomPreferenceTheme, true)
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
     }
