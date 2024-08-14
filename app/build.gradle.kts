55cf0944659a365c19ddc979e0fc5ec4751f8b73
askariya <arunalukari@gmail.com>
2024-08-14 08:29:33 -0600

    AAB Auto Versioning (#32)
    * Add Auto versioning for AAB
    
    ---------
    
    Co-authored-by: GitHub Actions <github-actions@github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "2.0"
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
