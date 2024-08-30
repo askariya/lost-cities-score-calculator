00e836b2b10017d9797b7794e573500384279aec
askariya <arunalukari@gmail.com>
2024-08-29 22:46:36 -0600

    Custom Name Editing (#56)
    * Add popup for entering custom names
    
    * Extend name length allowed and add restriction to name length setting in settingsframent
    
    * Restore portrait mode restriction

--- app/src/main/java/com/askariya/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/askariya/lostcitiesscorecalculator/ui/settings/SettingsFragment.kt
@@ -1,11 +1,29 @@
 package com.askariya.lostcitiesscorecalculator.ui.settings
 
 import android.os.Bundle
+import android.text.InputFilter
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.askariya.lostcitiesscorecalculator.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val player1NamePreference = findPreference<EditTextPreference>("player1name")
+        val player2NamePreference = findPreference<EditTextPreference>("player2name")
+
+        // Set character limit (e.g., 10 characters) for player1name and player2name
+        val maxLength = 20
+        val filterArray = arrayOf(InputFilter.LengthFilter(maxLength))
+
+        player1NamePreference?.setOnBindEditTextListener { editText ->
+            editText.filters = filterArray
+        }
+
+        player2NamePreference?.setOnBindEditTextListener { editText ->
+            editText.filters = filterArray
+        }
     }
 }
--- app/src/main/java/com/askariya/lostcitiesscorecalculator/ui/utils/DialogUtils.kt
+++ app/src/main/java/com/askariya/lostcitiesscorecalculator/ui/utils/DialogUtils.kt
@@ -4,10 +4,13 @@ import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.text.Html
+import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import com.askariya.lostcitiesscorecalculator.R
+import com.google.android.material.textfield.TextInputEditText
 
 object DialogUtils {
     fun showConfirmationDialog(context: Context,
@@ -70,6 +73,49 @@ object DialogUtils {
             positiveButton.setOnClickListener {
                 positiveButton.performHapticFeedback(android.view.HapticFeedbackConstants.VIRTUAL_KEY)
                 onConfirm()
+                dialog.dismiss() // Close the dialog
+            }
+        }
+        dialog.show()
+    }
+
+    fun showInputDialog(
+        context: Context,
+        title: String,
+        message: String,
+        positiveButtonText: String,
+        negativeButtonText: String,
+        onConfirm: (String) -> Unit
+    ) {
+        // Inflate the custom layout for the dialog
+        val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_edit_text, null)
+        val editText = dialogView.findViewById<TextInputEditText>(R.id.edit_text)
+
+        val builder = AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(Html.fromHtml(message, Html.FROM_HTML_MODE_COMPACT))
+            .setView(dialogView) // Set the custom view for the dialog
+            .setPositiveButton(positiveButtonText, null)
+            .setNegativeButton(negativeButtonText) { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.setOnShowListener {
+            val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE)
+            val negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE)
+
+            // Set a click listener to perform haptic feedback and handle the input
+            positiveButton.setOnClickListener {
+                positiveButton.performHapticFeedback(android.view.HapticFeedbackConstants.VIRTUAL_KEY)
+                val inputText = editText.text.toString()
+                onConfirm(inputText) // Pass the input text to the onConfirm callback
+                dialog.dismiss() // Close the dialog
+            }
+
+            // Set a click listener to perform haptic feedback and dismiss the dialog
+            negativeButton.setOnClickListener {
+                negativeButton.performHapticFeedback(android.view.HapticFeedbackConstants.VIRTUAL_KEY)
                 dialog.dismiss() // Close the dialog
             }
         }
